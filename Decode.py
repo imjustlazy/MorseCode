8ebca83874d48d668c776481f651459ccd521e2b
imjustlazy <wang0shijun@gmail.com>
2018-06-16 15:28:40 +0800

    add a decode  comment

--- Decode.py
+++ Decode.py
@@ -2,6 +2,9 @@ from Morse import MORSE_DECODE
 from Corpus import WORDS, MAX_WORD_LEN
 
 def try_reconginize(words_saw , char_left):
+    """
+    print lists of possible word-list from a list of characters
+    """
     if len(char_left) == 0:
         print(words_saw)
         return
@@ -14,7 +17,7 @@ def try_reconginize(words_saw , char_left):
 def decode(code_string):
     """
     returns a list of possible decode ways
-    every word in every candidate word list truly exists
+    every word in every candidate word-list truly exists
     """
     code_list = code_string.split(" ")
     characters = ""
