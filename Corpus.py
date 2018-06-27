708ae8d16574f45cd8794ec7676cb2d3f18c1d5c
imjustlazy <2628465713@qq.com>
2018-06-27 21:47:05 +0800

    change indentation

--- Corpus.py
+++ Corpus.py
@@ -6,21 +6,27 @@ test_string = '''Alice was beginning to get very tired of sitting by her sister 
  thought Alice "without pictures or conversation?"'''
 
 def load_words():
-	guaranty = set()
-	# print(vars())
-	# print(dir())
-	# print(locals())
-	if "test_string" in globals().keys():
-		guaranty = set(re.findall(r'[a-z]+', test_string.lower()))
-	print(guaranty)
-	ALL = set(re.findall(r'[a-z]+', open('Data/big.txt').read().lower()))
-	LONG = set(x for x in ALL if len(x) >= 4)
-	CORPUS = set(re.findall(r'[a-z]+', open('Data/corpus.txt').read().lower()))
-	SHORT = set(x for x in CORPUS if len(x) <= 3)
-	# print(SHORT)
-	return LONG | SHORT | guaranty
+    guaranty = set()
+    # print(vars())
+    # print(dir())
+    # print(locals())
+    if "test_string" in globals().keys():
+        guaranty = set(re.findall(r'[a-z]+', test_string.lower()))
+    print(guaranty)
+    ALL = set(re.findall(r'[a-z]+', open('Data/big.txt').read().lower()))
+    LONG = set(x for x in ALL if len(x) >= 4)
+    CORPUS = set(re.findall(r'[a-z]+', open('Data/corpus.txt').read().lower()))
+    SHORT = set(x for x in CORPUS if len(x) <= 3)
+    # print(SHORT)
+    return LONG | SHORT | guaranty
 
-WORDS = load_words()
+def load_words_test():
+    guaranty = set()
+    if "test_string" in globals().keys():
+        guaranty = set(re.findall(r'[a-z]+', test_string.lower()))
+    return guaranty
+
+WORDS = load_words_test()
 
 # A set of all possible prefixes of English words.
 WORD_PREFIX = set(word[:j+1] for word in WORDS for j in range(len(word)))
--- Decode.py
+++ Decode.py
@@ -15,7 +15,7 @@ def try_reconginize(words_saw , char_left):
         if len(words_saw) < len(min_words_list):
             min_words_list = words_saw.copy()
         return
-    for i in range(min(len(char_left), MAX_WORD_LEN)):
+    for i in range(min(len(char_left), MAX_WORD_LEN), 0, -1):
         if char_left[:i+1] in WORDS:
             words_saw.append(char_left[:i+1])
             try_reconginize(words_saw, char_left[i+1:])
@@ -39,30 +39,30 @@ def decode(code_string):
 
 
 def decode_nospace(words_saw, chars_saw, code_left):
-	"""
-	this should be a recursive function
-	words_saw: list, thw words list recognized, every word truly exists
-	chars_saw: string, the characters list recognizes, should be prefix of a word
-	code_left: string, the morse code left
-	"""
-	# print("words_saw:", words_saw, "chars_saw:", chars_saw, "code_left:", code_left)
-	if len(code_left) == 0:
-		if chars_saw in WORDS:
-			words_saw.append(chars_saw)
-			print(words_saw)
-		return
-	if len(chars_saw) > MAX_WORD_LEN:
-		return
-	if len(words_saw) > 2 and words_saw[-1] == words_saw[-2]:
-		return
-	if len(words_saw) > 4 and len("".join(words_saw[-7:])) < 15:
-		return
-	if chars_saw in WORDS:
-		words_saw.append(chars_saw)
-		decode_nospace(words_saw, "", code_left)
-		words_saw.pop()
-	for length in range(1, min(len(code_left), MAX_CODE_LEN) +1):
-		if code_left[:length] in MORSE_DECODE:
-			tmpchars = chars_saw + MORSE_DECODE[code_left[:length]]
-			if tmpchars in WORD_PREFIX:
-				decode_nospace(words_saw, tmpchars, code_left[length:])
+    """
+    this should be a recursive function
+    words_saw: list, thw words list recognized, every word truly exists
+    chars_saw: string, the characters list recognizes, should be prefix of a word
+    code_left: string, the morse code left
+    """
+    # print("words_saw:", words_saw, "chars_saw:", chars_saw, "code_left:", code_left)
+    if len(code_left) == 0:
+        if chars_saw in WORDS:
+            words_saw.append(chars_saw)
+            print(words_saw)
+        return
+    if len(chars_saw) > MAX_WORD_LEN:
+        return
+    if len(words_saw) > 2 and words_saw[-1] == words_saw[-2]:
+        return
+    if len(words_saw) > 4 and len("".join(words_saw[-7:])) < 15:
+        return
+    if chars_saw in WORDS:
+        words_saw.append(chars_saw)
+        decode_nospace(words_saw, "", code_left)
+        words_saw.pop()
+    for length in range(1, min(len(code_left), MAX_CODE_LEN) +1):
+        if code_left[:length] in MORSE_DECODE:
+            tmpchars = chars_saw + MORSE_DECODE[code_left[:length]]
+            if tmpchars in WORD_PREFIX:
+                decode_nospace(words_saw, tmpchars, code_left[length:])
